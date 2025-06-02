c042c3fc2f0c4e0767fcdebf75e2c8e25d8f98c4
sundarg1502 <sundarg1502@gmail.com>
2025-06-02 06:22:15 +0530

    Ass 5 Completed

--- Assignment5/Recursion.java
+++ Assignment5/Recursion.java
@@ -2,14 +2,20 @@ package Assignment5;
 
 public class Recursion {
     public static int count(int n){
-        int counter=0;
-        counter++;
-        if(n<0){
-            return 0;
+        if(n<10){
+            return 1;
         }
-        int rn=n/2;
-        count(rn);
-        return counter;
+        return 1+count(n/10);
+    }
+    public static boolean isPalindrome(String str,int start,int end){
+        if(start>=end){
+            return true;
+        }
+        if(str.charAt(start)!=str.charAt(end)){
+            return false;
+        }
+
+        return isPalindrome(str,start+1,end-1);
     }
     public static int pow(int n,int p){
         if(p<1){
@@ -19,7 +25,11 @@ public class Recursion {
     }
     public static void main(String[] args){
         // task 1
-        System.out.println(count(234));
+        System.out.println(count(23344));
+
+        // task 2
+        // String s = "madddam";
+        // System.out.println(isPalindrome(s,0,s.length()-1));
         // task 3
         // System.out.println(pow(3,3));
     }
