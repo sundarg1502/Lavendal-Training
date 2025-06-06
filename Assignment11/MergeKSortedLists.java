49145227e5f02ff3b3dacb3e6c38c93d715e87c4
sundarg1502 <sundarg1502@gmail.com>
2025-06-06 09:56:15 +0530

    Pending Submitted

---
+++ Assignment11/MergeKSortedLists.java
@@ -0,0 +1,64 @@
+package Assignment11;
+
+
+1) Merge k Sorted Lists
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0)
+            return null;
+        int interval = 1;
+        while (interval < lists.length) {
+            for (int i = 0; i + interval < lists.length; i = i + interval * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+        return lists[0];
+    }
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
+
+2) Delete Node in a Linked List
+
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+3)  Remove Duplicates from Sorted List
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val)
+                current.next = current.next.next;
+            else
+                current = current.next;
+        }
+        return head;
+    }
+}
